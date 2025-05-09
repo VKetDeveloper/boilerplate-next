1b200ed6060193881e51e1c514cc01399daba75c
Creat <ymandai@creator-net.com>
2025-05-10 08:49:45 +0900

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const nextConfig: NextConfig = {
   i18n: {
     locales: ['ja', 'en'], // 対応する言語
     defaultLocale: 'ja',   // デフォルト言語
-    localeDetection: true, // 自動言語検出（必要に応じて false に）
+    localeDetection: false, // 自動言語検出（必要に応じて false に）
   },
 };
 
